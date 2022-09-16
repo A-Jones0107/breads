638a129367235e59668a7731ed422ce3e4690b3d
alexis jones <ayecjones12@gmail.com>
2022-09-15 19:20:37 -0700

    sending index and show data

---
+++ server.js
@@ -0,0 +1,34 @@
+// DEPENDENCIES
+const express = require('express')
+
+// CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+console.log(PORT)
+
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('Welcome to an Awesome App about Breads!')
+})
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
+  
+
+  // Breads
+  const breadsController = require('./controllers/breads_controller.js')
+  app.use('/breads', breadsController)
+  
+// LISTEN
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
+})
